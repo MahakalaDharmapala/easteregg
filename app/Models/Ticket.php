3d048679674eabd427c5b18093cd024e1bc0617e
JuanCarlosHernandezLomas <hernandez.lomas.juan102@gmail.com>
2022-04-20 00:50:30 -0500

    cambios en ticket

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'game_id',
+        'date_purchase',
+        'batch'
+    ];
+
+
+public function game(){
+    return $this->belongsTo('App\Models\Game');
 }
+
+public function sale(){
+    return $this->hasMany('App\Models\Sale');
+}
+}
+
